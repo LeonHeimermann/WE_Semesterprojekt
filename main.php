5be01b4797088bc1e018b050faa56cb215ffc915
LeonHeimermann <leonheimermann@gmail.com>
2021-12-23 19:03:40 +0100

    Aufbau der Vue-component kann jetzt von einer JSON geladen werden

--- main.php
+++ main.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
-<html lang="de">
+<script>
+	var json_data;
+	fetch("./data.json")
+	.then(response => {
+		json_data_tmp = response.json();
+		json_data_tmp.then(data => { 
+			json_data = data 
+		});
+	});
+</script>
+<html lang="de">  
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,91 +19,7 @@
   <body>
 	<script src="https://unpkg.com/vue"></script>
 	<div id="vue-app-one">
-		<navigator id="d_navigator" :buttons="[
-			{name: 'Einführung', sub: [  
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]}, 
-			{name: 'CSS 1', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'CSS 2', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'JavaScript', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'DOM', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'ECMAScript', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'Functional', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'Async', sub: [
-								{name: 'xd'}, 
-								{name: 'toll'}
-								]},
-			{name: 'SVG', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'TypeScript', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'Vue', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'PHP', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'Security', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]},
-			{name: 'Vue3', sub: [
-								{name: 'A1'}, 
-								{name: 'A2'},
-								{name: 'A3'}, 
-								{name: 'A4'}
-								]}
-			]">
-		</navigator>
+		<navigator id="d_navigator"></navigator>
 	</div>
 	<div id="d_main">
 		<iframe id="if_main" src="./index.php"></iframe>
